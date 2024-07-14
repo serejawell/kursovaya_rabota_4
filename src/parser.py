9f5418362484e34f482db47a0f9e52b8a97c7059
serejawell <sev2316@yandex.ru>
2024-07-14 23:30:04 +0300

    adding Parser class

--- src/parser.py
+++ src/parser.py
@@ -1,41 +1,23 @@
-from abc import ABC, abstractmethod
 import requests
+from abc import ABC, abstractmethod
 
 
-class AbstractApi(ABC):
-
+class Api(ABC):
     @abstractmethod
-    def get_url(self):
-        pass
-
-    def get_response(self):
-        pass
-
     def get_vacancies(self):
         pass
 
 
-class HH(AbstractApi):
-
+class HhAPI:
     def __init__(self):
-        self.__url = 'https://api.hh.ru/vacancies'
+        self._url = "https://api.hh.ru/vacancies"
 
-    def get_url(self):
-        return self.__url
+    def get_vacancies(self, text, per_page):
+        params = {
+            "text": text,
+            "per_page": per_page
+        }
+        response = requests.get(self._url, params=params)
+        response.raise_for_status()
+        return response.json()["items"]
 
-    def get_response(self):
-        """Ожидание ответа от сервера"""
-        response = requests.get(self.__url)
-        return response
-
-    def get_vacancies(self, per_page: int, text: str = ''):
-        """Получает вакансии по нашему API"""
-        self.per_page = per_page
-        self.text = text
-        params = {'text': self.text, 'page': 0, "per_page": self.per_page}
-        response = requests.get(self.__url, params)
-        return response.json()['items']
-
-a = HH()
-print(a.get_response())
-print(a.get_vacancies(1,'developer'))
